abbab13c549c9b784d44742d9bd89e5657d5adbe
Rodolfo <rodolfo.nascimento@dtidigital.com.br>
2021-05-06 15:26:36 -0300

    inicio

---
+++ src/main/java/com/aped/ApedApplication.java
@@ -0,0 +1,13 @@
+package com.aped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aped/ApedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
